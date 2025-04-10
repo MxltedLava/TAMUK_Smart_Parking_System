b1d8ecfae747d09094c562c0b5018ecbae8830f4
Your Name <you@example.comdestiny@admin.comgit cofig --global user.name Your>
2025-04-10 09:04:00 -0500

    hello

--- smart-parking-backend/sensors/app/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py in sensors or wherever you created the view
-from django.urls import path
-from .views import take_picture_view
-
-urlpatterns = [
-    path('take-picture/', take_picture_view, name='take_picture'),
-]
